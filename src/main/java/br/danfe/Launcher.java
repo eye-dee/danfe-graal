255c52618a0011ad15ea04c87f7f1f2b445fa4ab
sausageRoll <drozdovigor1996@gmail.com>
2019-12-31 00:16:26 +0100

    temp commit

--- src/main/java/br/danfe/Launcher.java
+++ src/main/java/br/danfe/Launcher.java
@@ -8,6 +8,7 @@ public class Launcher {
 
     public static void main(String[] args) {
         System.out.println("Launcher rubyHelloWorld started");
+        System.out.println("Launcher rubyHelloWorld started");
         Context polyglot = Context.newBuilder().allowAllAccess(true).build();
         Value array = polyglot.eval("ruby", "[1,2,42,4]");
         int result = array.getArrayElement(2).asInt();
