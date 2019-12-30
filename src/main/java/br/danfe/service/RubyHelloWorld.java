643877f71e031175d763f6e71f107310c5af34be
sausageRoll <drozdovigor1996@gmail.com>
2019-12-30 22:58:00 +0100

    install graal ruby correctly

--- src/main/java/br/danfe/service/RubyHelloWorld.java
+++ src/main/java/br/danfe/service/RubyHelloWorld.java
@@ -1,17 +1,21 @@
 package br.danfe.service;
 
 import javax.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class RubyHelloWorld {
 
     @PostConstruct
     public void rubyHelloWorld() {
-        try (Context context = Context.create()) {
-            context.eval("ruby", "puts 'Hello, world!'");
-        }
+        Context polyglot = Context.newBuilder().allowAllAccess(true).build();
+        Value array = polyglot.eval("ruby", "[1,2,42,4]");
+        int result = array.getArrayElement(2).asInt();
+        log.info("result {}", result);
     }
 
 }
