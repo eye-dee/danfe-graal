5d1aaec5a0d913ba1c28a0020f4217a8095241f3
sausageRoll <drozdovigor1996@gmail.com>
2019-12-30 23:32:50 +0100

    temp commit

--- src/main/java/br/danfe/Launcher.java
+++ src/main/java/br/danfe/Launcher.java
@@ -2,16 +2,16 @@ package br.danfe;
 
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Value;
+import org.springframework.boot.SpringApplication;
 
 public class Launcher {
 
     public static void main(String[] args) {
-//        SpringApplication.run(Launcher.class);
-
         System.out.println("rubyHelloWorld started");
         Context polyglot = Context.newBuilder().allowAllAccess(true).build();
         Value array = polyglot.eval("ruby", "[1,2,42,4]");
         int result = array.getArrayElement(2).asInt();
         System.out.println("result {}" + result);
+        SpringApplication.run(Launcher.class);
     }
 }
--- src/main/java/br/danfe/service/RubyHelloWorld.java
+++ src/main/java/br/danfe/service/RubyHelloWorld.java
@@ -4,11 +4,12 @@ import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-public class RubyHelloWorld {
+public class RubyHelloWorld implements CommandLineRunner {
 
     @PostConstruct
     public void rubyHelloWorld() {
@@ -19,4 +20,12 @@ public class RubyHelloWorld {
         log.info("result {}", result);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("rubyHelloWorld runner started");
+        Context polyglot = Context.newBuilder().allowAllAccess(true).build();
+        Value array = polyglot.eval("ruby", "[1,2,42,4]");
+        int result = array.getArrayElement(2).asInt();
+        log.info("runner result {}", result);
+    }
 }
