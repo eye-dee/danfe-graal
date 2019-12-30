3effa28d424e2f774865ba5f2dee06ccada28697
sausageRoll <drozdovigor1996@gmail.com>
2019-12-31 00:27:56 +0100

    set Launcher

--- src/main/java/br/danfe/Launcher.java
+++ src/main/java/br/danfe/Launcher.java
@@ -1,18 +1,12 @@
 package br.danfe;
 
-import org.graalvm.polyglot.Context;
-import org.graalvm.polyglot.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class Launcher {
 
     public static void main(String[] args) {
-        System.out.println("Launcher rubyHelloWorld started");
-        System.out.println("Launcher rubyHelloWorld started");
-        Context polyglot = Context.newBuilder().allowAllAccess(true).build();
-        Value array = polyglot.eval("ruby", "[1,2,42,4]");
-        int result = array.getArrayElement(2).asInt();
-        System.out.println("Launcher result {}" + result);
-//        SpringApplication.run(Launcher.class);
+        SpringApplication.run(Launcher.class);
     }
 }
