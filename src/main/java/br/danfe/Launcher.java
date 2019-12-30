1155b2e7061937d59da5d3a479db8e2ecc025ba3
sausageRoll <drozdovigor1996@gmail.com>
2019-12-30 23:39:31 +0100

    temp commit

--- src/main/java/br/danfe/Launcher.java
+++ src/main/java/br/danfe/Launcher.java
@@ -3,7 +3,9 @@ package br.danfe;
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Launcher {
 
     public static void main(String[] args) {
