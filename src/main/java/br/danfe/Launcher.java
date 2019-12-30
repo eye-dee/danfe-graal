3d81d842c0b2986234be8a2d93e184f7dac928b3
sausageRoll <drozdovigor1996@gmail.com>
2019-12-31 00:08:45 +0100

    temp commit

--- src/main/java/br/danfe/Launcher.java
+++ src/main/java/br/danfe/Launcher.java
@@ -1,14 +1,17 @@
 package br.danfe;
 
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
+
 //@SpringBootApplication
 public class Launcher {
 
     public static void main(String[] args) {
         System.out.println("Launcher rubyHelloWorld started");
-//        Context polyglot = Context.newBuilder().allowAllAccess(true).build();
-//        Value array = polyglot.eval("ruby", "[1,2,42,4]");
-//        int result = array.getArrayElement(2).asInt();
-//        System.out.println("Launcher result {}" + result);
+        Context polyglot = Context.newBuilder().allowAllAccess(true).build();
+        Value array = polyglot.eval("ruby", "[1,2,42,4]");
+        int result = array.getArrayElement(2).asInt();
+        System.out.println("Launcher result {}" + result);
 //        SpringApplication.run(Launcher.class);
     }
 }
